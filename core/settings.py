05b75d952fcf4192ccf04b9b4ab76260608eac78
Zekiyev <zekiyev014@gmail.com>
2023-04-05 09:29:18 +0400

    testing

--- core/settings.py
+++ core/settings.py
@@ -77,23 +77,23 @@ WSGI_APPLICATION = "core.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#        'NAME': 'evial', 
-#        'USER': 'postgres', 
-#        'PASSWORD': 'changeme',#'12345',
-#        'HOST': '94.20.154.220',#, 127.0.0.1
-#        'PORT': '5432',
-#    }
-#}
-import os
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'evial', 
+        'USER': 'postgres', 
+        'PASSWORD': 'changeme',#'12345',
+        'HOST': '94.20.154.220',#, 127.0.0.1
+        'PORT': '5432',
     }
 }
+#import os
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
